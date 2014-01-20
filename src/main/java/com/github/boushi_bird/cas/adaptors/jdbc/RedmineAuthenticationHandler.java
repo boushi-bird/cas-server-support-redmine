d5419a6255934a7b6fe222e9e28805b449ccb861
boushi-bird <kaz.8tori@gmail.com>
2014-01-21 03:27:59 +0900

    CAS認証にRedmineのDBを使用するためのモジュール作成

---
+++ src/main/java/com/github/boushi_bird/cas/adaptors/jdbc/RedmineAuthenticationHandler.java
@@ -0,0 +1,87 @@
+package com.github.boushi_bird.cas.adaptors.jdbc;
+
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.jasig.cas.adaptors.jdbc.AbstractJdbcUsernamePasswordAuthenticationHandler;
+import org.jasig.cas.authentication.handler.AuthenticationException;
+import org.jasig.cas.authentication.handler.DefaultPasswordEncoder;
+import org.jasig.cas.authentication.principal.UsernamePasswordCredentials;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+
+public class RedmineAuthenticationHandler extends
+		AbstractJdbcUsernamePasswordAuthenticationHandler {
+	private static final String WHERE = " WHERE login = ? AND status = 1 AND auth_source_id IS NULL";
+	private static final String SQL = "SELECT hashed_password, salt FROM users"
+			+ WHERE;
+	private static final String SQL_WITHOUT_SALT = "SELECT hashed_password FROM users"
+			+ WHERE;
+
+	private static final String COL_SALT = "salt";
+	private static final String COL_HASHED_PASSWORD = "hashed_password";
+
+	private boolean useSalt;
+
+	public RedmineAuthenticationHandler() {
+		this.useSalt = true;
+		this.setPasswordEncoder(new DefaultPasswordEncoder("SHA"));
+	}
+
+	@Override
+	protected final boolean authenticateUsernamePasswordInternal(
+			final UsernamePasswordCredentials credentials)
+			throws AuthenticationException {
+		final String username = getPrincipalNameTransformer().transform(
+				credentials.getUsername());
+		final String password = credentials.getPassword();
+		final String encryptedPassword = this.getPasswordEncoder().encode(
+				password);
+		if (this.useSalt) {
+			return this.authenticateWithSalt(username, encryptedPassword);
+		} else {
+			return this.authenticateWithoutSalt(username, encryptedPassword);
+		}
+	}
+
+	private boolean authenticateWithSalt(final String username,
+			final String encryptedPassword) {
+		final Map<String, Object> user;
+		try {
+			user = this.query(SQL, username);
+		} catch (final IncorrectResultSizeDataAccessException e) {
+			return false;
+		}
+		final String salt = String.valueOf(user.get(COL_SALT));
+		final String hashedPassword = String.valueOf(user
+				.get(COL_HASHED_PASSWORD));
+		if (StringUtils.isNotEmpty(salt)) {
+			return hashedPassword.equals(this.getPasswordEncoder().encode(
+					salt + encryptedPassword));
+		} else {
+			return hashedPassword.equals(encryptedPassword);
+		}
+	}
+
+	private boolean authenticateWithoutSalt(final String username,
+			final String encryptedPassword) {
+		final Map<String, Object> user;
+		try {
+			user = this.query(SQL_WITHOUT_SALT, username);
+		} catch (final IncorrectResultSizeDataAccessException e) {
+			return false;
+		}
+		final String hashedPassword = String.valueOf(user
+				.get(COL_HASHED_PASSWORD));
+		return hashedPassword.equals(encryptedPassword);
+	}
+
+	@SuppressWarnings("deprecation")
+	private Map<String, Object> query(final String sql, final String username) {
+		return getJdbcTemplate().queryForMap(sql, username);
+	}
+
+	public final void setUseSalt(boolean useSalt) {
+		this.useSalt = useSalt;
+	}
+
+}
